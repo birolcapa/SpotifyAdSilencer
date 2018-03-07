b7b99a59ce9b45ac6fc7155fd96c452ef18b4e11
Birol Capa <birol.capa@siemens.com>
2018-03-07 20:02:48 +0300

    I added task routine.

--- SpotifyAdSilencer/Program.cs
+++ SpotifyAdSilencer/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 using log4net;
 
 namespace SpotifyAdSilencer
@@ -16,6 +18,24 @@ namespace SpotifyAdSilencer
             Console.WriteLine("Hold esc key to stop");
             do
             {
+                while (!Console.KeyAvailable)
+                {
+                    // Search for Spotify Player Window Class Name
+                    Dictionary<IntPtr, string> windowsByClassName = 
+                        spotifyPlayerWrapper.GetSpotifyPlayerWindowByClassName();
+
+                    // If it is found, do following
+                    if (windowsByClassName.Count != 0)
+                    {
+                    }
+                    // If it is not found, log a message to screen
+                    else
+                    {
+                        Console.WriteLine("Error: windowsByClassName.Count == 0");
+                    }
+                    // Do this control every ten seconds
+                    Thread.Sleep(10000);
+                }
             } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
     }
